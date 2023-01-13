ace46b7fa109130521f96e95fe3ea3fe61ac469a
Jean Michel Biz <jeanmbiz@hotmail.com>
2023-01-13 08:51:32 -0300

    api deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,27 +1,45 @@
-import { DataSource } from "typeorm";
-import path from "path";
 import "dotenv/config";
+import path from "path";
+import "reflect-metadata";
+import { DataSource, DataSourceOptions } from "typeorm";
 
-const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"],
-      }
-    : {
-        type: "postgres",
-        host: process.env.PGHOST,
-        port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
-        synchronize: false,
-        entities: [path.join(__dirname, "./entities/**.{js,ts}")],
-        migrations: [path.join(__dirname, "./migrations/**.{js,ts}")],
-      }
-);
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
+  const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
 
-export default AppDataSource;
+  const nodeEnv: string = process.env.NODE_ENV;
+
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
+
+  if (nodeEnv === 'test'){
+    return {
+      type: "sqlite",
+      database: ":memory:",
+      synchronize: true,
+      entities: ["src/entities/*.ts"],
+    }
+  }
+
+  return {
+    type: "postgres",
+    host: process.env.PGHOST,
+    port: parseInt(process.env.PGPORT),
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
+};
+
+const AppDataSource = new DataSource(dataSourceConfig());
+
+export default AppDataSource
