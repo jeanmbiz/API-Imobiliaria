5c614be206ab5725dbd0017d079509a75beb515e
Jean <103115353+jeanmbiz@users.noreply.github.com>
2024-08-05 09:07:44 -0300

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,6 +3,7 @@ import path from "path";
 import "reflect-metadata";
 import { DataSource, DataSourceOptions } from "typeorm";
 
+
 const dataSourceConfig = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "./entities/*.{js,ts}");
   const migrationsPath: string = path.join(__dirname, "./migrations/*.{js,ts}");
